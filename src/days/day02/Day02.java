84f8072c2548f17a3beb51ac817b9f2d4ec3f64d
NikolasDmn <diamantonikolas@gmail.com>
2022-12-02 06:45:20 +0100

    Day 02

---
+++ src/days/day02/Day02.java
@@ -0,0 +1,62 @@
+package days.day02;
+
+import days.Day;
+
+import java.util.ArrayList;
+
+
+public class Day02 extends Day {
+    public Day02() {
+        super(02, "sample.txt", "input.txt");
+    }
+
+    public Day02(String input_path, String sample_path) {
+        super(02, input_path, sample_path);
+    }
+
+    public char[][] parseInput(String input) {
+        String[] lines = input.split("\\r?\\n");
+        char[][] data = new char[lines.length][2];
+        for(int i =0; i<lines.length;i++){
+            data[i][0] = lines[i].charAt(0);
+            data[i][1] = lines[i].charAt(2);
+        }
+        return data;
+    }
+
+    public String part1(String input) {
+        char[][] data = parseInput(input);
+        int score = 0;
+        for(char[] game:data){
+            score +=(game[1] - 'X' +1);
+            if (game[0]-'A' ==  game[1]-'X'){
+                score += 3;
+            }
+            else if((game[0]-'A' + 1)%3 ==game[1]-'X') {
+                score+=6;
+            }
+        }
+        return String.valueOf(score);
+    }
+
+    public String part2(String input) {
+        char[][] data = parseInput(input);
+        int score = 0;
+        for(char[] game:data){
+            score +=3*(game[1]-'X');
+//          I don't even know how I found this equation.
+//          I thought there would be one, so I did a lot of trial and error.
+            score += (game[0]-'A' + game[1]-'X' +2)%3+1;
+        }
+        return String.valueOf(score);
+    }
+
+    @Override
+    protected String[] getAnswer(String input) {
+        return new String[]{this.part1(input), this.part2(input)};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Day02());
+    }
+}
