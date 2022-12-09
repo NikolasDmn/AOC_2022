2f61989f707baa3beed2203caeb632556409a02f
NikolasDmn <diamantonikolas@gmail.com>
2022-12-09 15:40:28 +0100

    Day 09 Part 2

--- src/days/day09/Day09.java
+++ src/days/day09/Day09.java
@@ -10,106 +10,7 @@ import java.util.stream.Collectors;
 
 
 public class Day09 extends Day {
-    private class Pos{
-        public int x=0,y=0;
-        public Pos(){}
-        public void up(){
-            x++;
-        }
-        public void down(){
-            x--;
-        }
-        public void right(){
-            y++;
-        }
-        public void left(){
-            y--;
-        }
 
-        public String toString() {
-            return String.format("(%d,%d)",y,x);
-        }
-    }
-    private class Rope{
-        HashSet <String> tailLocations = new HashSet<>();
-        private final Pos head = new Pos();
-        private final Pos tail = new Pos();
-        public Rope(){
-        }
-        private int distance(){
-            return (Math.abs(head.x-tail.x) + Math.abs(head.y-tail.y));
-        }
-        public void move(char direction, int steps){
-            switch(direction){
-                case 'U':
-                    goUp(steps);
-                    break;
-                case 'D':
-                    goDown(steps);
-                    break;
-                case 'R':
-                    goRight(steps);
-                    break;
-                case 'L':
-                    goLeft(steps);
-                    break;
-                default:
-                    System.out.println("Wrong direction");
-            }
-        }
-        private void handleTail() {
-            if(tail.x == head.x && distance()==2) {
-                tail.y += head.y > tail.y ? 1 : -1;
-                return;
-            };
-            if(tail.y == head.y && distance()==2) {
-                tail.x += head.x > tail.x ? 1 : -1;
-                return;
-            }
-            if(distance() !=3) return;
-            tail.y += head.y > tail.y ? 1 : -1;
-            tail.x += head.x > tail.x ? 1 : -1;
-        }
-
-        private void goUp(int steps) {
-            for(int i = 0; i< steps; i++) {
-                head.up();handleTail();
-                addLocation();
-            }
-        }
-
-
-        private void goDown(int steps) {
-            for(int i = 0; i< steps; i++) {
-                head.down();handleTail();
-                addLocation();
-            }
-        }
-
-        private void goRight(int steps) {
-            for(int i = 0; i< steps; i++) {
-                head.right();handleTail();
-                addLocation();
-            }
-
-        }
-
-        private void goLeft(int steps) {
-            for(int i = 0; i< steps; i++) {
-                head.left();handleTail();
-                addLocation();
-            }
-        }
-
-        private void addLocation() {
-            tailLocations.add(String.valueOf(tail));
-        }
-
-        public HashSet<String> getLocations(){
-            return tailLocations;
-        }
-
-    }
     public Day09() {
         super(9, "sample.txt", "input.txt");
     }
@@ -121,21 +22,77 @@ public class Day09 extends Day {
     public List<String[]> parseInput(String input) {
         return input.lines().map(l -> l.split(" ")).collect(Collectors.toList());
     }
+    public class Point{
+        public int x = 0;
+        public int y = 0;
+        public Point(){}
+
+        @Override
+        public String toString() {
+            return String.format("(%d,%d)",x,y);
+        }
+    }
+    public void shift(String direction,Point point){
+        switch (direction){
+            case "U":
+                point.y++;
+                break;
+            case "D":
+                point.y--;
+                break;
+            case "R":
+                point.x++;
+                break;
+            case "L":
+                point.x--;
+                break;
+            default:
+                System.out.println("Incorrect direction: " + direction);
+        }
+    }
+    public int getPlaces(List<String[]> commands, int knotAmmount){
+        Point[] knots = new Point[knotAmmount];
+        for(int i = 0;i<knotAmmount;i++) knots[i] = new Point();
+        HashSet<String> positions = new HashSet<>();
+        for(String[] command: commands){
+            String direction = command[0];
+            int steps = Integer.parseInt(command[1]);
+            for(int i =0;i<steps;i++){
+                shift(direction,knots[0]);
+                moveTail(knots);
+                positions.add(String.valueOf(knots[knots.length-1]));
+            }
+        }
+
+        return positions.size();
+    }
+    public static float getTailStep(int val) {
+        return Math.max(-1, Math.min(1, val));
+    }
+    private void moveTail(Point[] knots) {
+        for(int i = 1;i<knots.length;i++){
+            Point prev = knots[i-1];
+            Point current = knots[i];
+            if (determineMove(prev,current)){
+                current.x += getTailStep(prev.x-current.x);
+                current.y += getTailStep(prev.y-current.y);
+            }
+        }
+    }
+
+    private boolean determineMove(Point p1, Point p2) {
+        return Math.abs(p1.x-p2.x) > 1 || Math.abs(p1.y-p2.y)>1;
+    }
 
     public String part1(String input) {
-        Rope rope = new Rope();
-        List<String[]> data = parseInput(input);
-        for(String[] entry: data){
-            rope.move(entry[0].charAt(0), Integer.parseInt(entry[1]));
-        }
-        System.out.println(rope.getLocations());
-        return String.valueOf(rope.getLocations().size());
+       return String.valueOf(getPlaces(parseInput(input),2));
     }
+
 
     public String part2(String input) {
-        return "TBD";
-    }
+        return String.valueOf(getPlaces(parseInput(input),10));
 
+    }
     @Override
     protected String[] getAnswer(String input) {
         return new String[]{this.part1(input), this.part2(input)};
